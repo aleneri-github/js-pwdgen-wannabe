eef506c9e1d03276ec867ca30ff3c6e4eb59bff7
Alessandro <aleneri242@gmail.com>
2020-11-10 15:00:28 +0100

    Secondo commit

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,7 @@
 var name = prompt("Inserisci il tuo nome:");
 var surname = prompt("Inserisci il tuo cognome:");
 var favoriteColor = prompt("Indica il tuo colore preferito:");
+
+document.getElementById("name").innerHTML = name;
+document.getElementById("surname").innerHTML = surname;
+document.getElementById("favorite_color").innerHTML = favoriteColor;
